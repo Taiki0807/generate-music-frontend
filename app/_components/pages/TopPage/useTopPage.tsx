d01c116c238a51de5b768cf8e4335d5ca68cb2cf
Taiki0807 <hosokawataiki265@gmail.com>
2023-07-05 00:20:51 +0900

    feat: add custom hook for top page functionality

---
+++ app/_components/pages/TopPage/useTopPage.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import { postFetcher } from '@/utils/httpClient';
+
+export const useTopPage = () => {
+  const [input, setInput] = useState('');
+  const [loading, setLoading] = useState(false);
+  const handleChange = (value: string) => {
+    setInput(value);
+  };
+  const handleSubmit = async () => {
+    setLoading(true);
+    const requestBody = {
+      prompt_a: input,
+    };
+    try {
+      const data = await postFetcher(
+        '/v2/p-a7dd4047/generate-music/predict',
+        requestBody,
+        {
+          Authorization:
+            process.env.NEXT_PUBLIC_AUTH_PUBLIC_KEY,
+        }
+      );
+      console.log(data);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return {
+    handleChange,
+    handleSubmit,
+    loading,
+  };
+};
