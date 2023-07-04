bcfa9b5c195653e78c1afbb21c842d435b12f45d
Taiki0807 <hosokawataiki265@gmail.com>
2023-07-05 00:19:42 +0900

    feat: add loading state to Button component

--- app/_components/parts/Button/Button.tsx
+++ app/_components/parts/Button/Button.tsx
@@ -4,20 +4,34 @@ interface Props {
   color?: 'danger' | 'primary' | 'success';
   children: React.ReactNode;
   onClick?: () => void;
+  loading?: boolean;
 }
 
 export const Button = (props: Props): JSX.Element => {
   const color = props.color ?? 'primary';
+  const loading = props.loading ?? false;
   return (
     <div>
-      <button
-        className={`${style.button} ${
-          style[`button-${color}`]
-        }`}
-        onClick={props.onClick}
-      >
-        {props.children}
-      </button>
+      {loading ? (
+        <button
+          className={`${style.button} ${
+            style[`button-${color}`]
+          }`}
+          onClick={props.onClick}
+        >
+          loading...
+          <div className={style.loader}></div>
+        </button>
+      ) : (
+        <button
+          className={`${style.button} ${
+            style[`button-${color}`]
+          }`}
+          onClick={props.onClick}
+        >
+          {props.children}
+        </button>
+      )}
     </div>
   );
 };
